219e0064117d25057748fd4deebb7e81c4b5d6ab
DominMFD <mfdmatheus@hotmail.com>
2024-05-23 23:39:13 -0300

    test: oxygenation controller tests created

---
+++ tests/OxygenationTests/OxygenationController.spec.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+public class OxygenationControllerTest {
+    Mock<IOxygenationService> mockService = new Mock<IOxygenationService>();
+
+    [Fact]
+    public void AddOxygenationAndReturnStatusCode() {
+        var oxygenationDto = new OxygenationDto {
+            Date = DateTime.Now,
+            Value = 80,
+        };
+
+        var oxygenationModel = new OxygenationModel {
+            Id = 1,
+            Date = DateTime.Now,
+            Value = 80,
+        };
+
+        mockService.Setup(service => service.AddOxygenation(oxygenationDto)).Returns(oxygenationModel);
+        var controller = new OxygenationController(mockService.Object);
+
+        var result = controller.AddOxygenation(oxygenationDto) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(201, result.StatusCode);
+        Assert.Equal(oxygenationModel, result.Value);
+    }
+
+    [Fact]
+    public void GetOxygenationAndReturnStatusCode() {
+        var oxygenationModel = new OxygenationModel {
+            Id = 1,
+            Date = DateTime.Now,
+            Value = 80,
+        };
+
+        var oxygenationDto = new OxygenationDto {
+            Date = DateTime.Now,
+            Value = 80,
+        };
+
+        mockService.Setup(service => service.AddOxygenation(oxygenationDto)).Returns(oxygenationModel);
+        mockService.Setup(service => service.GetAOxygenation(1)).Returns(oxygenationModel);
+        var controller = new OxygenationController(mockService.Object);
+
+        controller.AddOxygenation(oxygenationDto);
+
+        var result = controller.GetAOxygenation(1) as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(oxygenationModel, result.Value);   
+    }  
+
+    [Fact]
+    public void ListAllOxygenationAndReturnStatusCode() {
+        var oxygenations = new List<OxygenationModel>{
+            new OxygenationModel() {
+                Id = 1,
+                Date = DateTime.Now,
+                Value = 80,
+            },
+            new OxygenationModel{
+                Id = 2,
+                Date = DateTime.Now,
+                Value = 75,
+            }
+        };
+
+        var oxygenationDto = new OxygenationDto{
+            Date = DateTime.Now,
+            Value = 80,
+        };
+        var oxygenationDto2 = new OxygenationDto{
+            Date = DateTime.Now,
+            Value = 75,
+        };
+
+        mockService.Setup(service => service.AddOxygenation(oxygenationDto));
+        mockService.Setup(service => service.AddOxygenation(oxygenationDto2));
+
+        mockService.Setup(service => service.ListAllOxygenation("date", "asc", 1)).Returns(oxygenations);
+
+        var controller = new OxygenationController(mockService.Object);
+        var result = controller.ListAllOxygenation() as ObjectResult;
+
+        Assert.NotNull(result);
+        Assert.Equal(200, result.StatusCode);
+        Assert.Equal(oxygenations, result.Value);
+    }
+}
